7ea6941ea10d595d928a1e12003acfc545181ab7
Diego Pagini <diego.pagini@neoris.com>
2022-11-30 17:35:30 +0100

    useForm

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -11,5 +11,14 @@ export const useForm = <T>(initialState: T) => {
 		}));
 	};
 
-	return { formData, onChange };
+	const resetForm = () => {
+		setFormData({ ...initialState });
+	};
+
+	const isValidEmail = (email: string) => {
+		const re =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	};
+	return { formData, onChange, resetForm, isValidEmail };
 };
--- src/03-forms/pages/ResgisterPage.tsx
+++ src/03-forms/pages/ResgisterPage.tsx
@@ -13,7 +13,7 @@ interface RegisterForm {
 }
 
 export const ResgisterPage = () => {
-	const { onChange, formData } = useForm<RegisterForm>({
+	const { onChange, formData, resetForm, isValidEmail } = useForm<RegisterForm>({
 		name: '',
 		email: '',
 		password1: '',
@@ -30,9 +30,25 @@ export const ResgisterPage = () => {
 		<div>
 			<h1>Resgister Page</h1>
 			<form noValidate onSubmit={onSubmit}>
-				<input name='name' type='text' placeholder='Name' value={name} onChange={onChange} />
+				<input
+					name='name'
+					type='text'
+					placeholder='Name'
+					value={name}
+					onChange={onChange}
+					className={`${name.trim().length <= 0 && 'has-error'}`}
+				/>
+				{name.trim().length <= 0 && <span>Este campo es requerido.</span>}
 
-				<input name='email' type='email' placeholder='Email' value={email} onChange={onChange} />
+				<input
+					name='email'
+					type='email'
+					placeholder='Email'
+					value={email}
+					onChange={onChange}
+					className={`${!isValidEmail(email) && 'has-error'}`}
+				/>
+				{!isValidEmail(email) && <span>Este campo es requerido.</span>}
 
 				<input
 					name='password1'
@@ -41,6 +57,10 @@ export const ResgisterPage = () => {
 					value={password1}
 					onChange={onChange}
 				/>
+				{password1.trim().length <= 0 && <span>Este campo es requerido.</span>}
+				{password1.trim().length < 6 && password1.trim().length > 0 && (
+					<span>Mínimo 6 caracteres.</span>
+				)}
 
 				<input
 					name='password2'
@@ -49,8 +69,15 @@ export const ResgisterPage = () => {
 					value={password2}
 					onChange={onChange}
 				/>
+				{password2.trim().length <= 0 && <span>Este campo es requerido.</span>}
+				{password2.trim().length > 0 && password2 !== password1 && (
+					<span>Deben de ser iguales.</span>
+				)}
 
 				<button type='submit'>Submit</button>
+				<button type='button' onClick={resetForm}>
+					Reset
+				</button>
 			</form>
 		</div>
 	);
