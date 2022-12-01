7e7f4912e2be6f2127ab272b74a99bbf7f993fc2
Diego Pagini <diegopaginidev@gmail.com>
2022-12-01 15:36:30 +0100

    more formik

--- src/03-forms/pages/FormikComponents.tsx
+++ src/03-forms/pages/FormikComponents.tsx
@@ -14,18 +14,22 @@ export const FormikComponents = () => {
 					firstName: '',
 					lastName: '',
 					email: '',
+					terms: false,
+					jobType: '',
 				}}
 				onSubmit={(values) => {
 					console.log(values);
 				}}
 				validationSchema={Yup.object({
 					firstName: Yup.string()
-						.max(15, 'Debe de tener 15 caracteres o menos')
-						.required('Requerido'),
+						.max(15, 'Debe de tener 15 caracteres o menos.')
+						.required('Requerido.'),
 					lastName: Yup.string()
-						.max(12, 'Debe de tener 12 caracteres o menos')
-						.required('Requerido'),
-					email: Yup.string().required('Requerido').email(),
+						.max(12, 'Debe de tener 12 caracteres o menos.')
+						.required('Requerido.'),
+					email: Yup.string().required('Requerido.').email(),
+					jobType: Yup.string().required().notOneOf(['it-jr'], 'Esta opción no es permitida.'),
+					terms: Yup.boolean().oneOf([true], 'Debe de aceptar los terminos y condiciones.'),
 				})}>
 				{(formik) => (
 					<Form>
@@ -40,6 +44,22 @@ export const FormikComponents = () => {
 						<label htmlFor='email'>Email Address</label>
 						<Field name='email' type='email' id='email' />
 						<ErrorMessage name='email' component={'span'} />
+
+						<label htmlFor='jobType'>Job Type</label>
+						<Field name='jobType' as='select' id='jobType'>
+							<option value={''}>Pick somenthing</option>
+							<option value='developer'>Developer</option>
+							<option value='designer'>Designer</option>
+							<option value='it-senior'>IT Senior</option>
+							<option value='it-jr'>IT Jr</option>
+						</Field>
+						<ErrorMessage name='jobType' component={'span'} />
+
+						<label htmlFor='terms'>
+							<Field name='terms' type='checkbox' id='terms' />
+							Terms and conditions
+						</label>
+						<ErrorMessage name='terms' component={'span'} />
 
 						<button type='submit'>Submit</button>
 					</Form>
